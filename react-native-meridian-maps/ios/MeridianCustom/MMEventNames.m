df4b59b8e64712eedc685c4d57214ca03d25ace4
Iliya Lyan <68940374+12ya@users.noreply.github.com>
2025-05-22 00:55:36 +0900

    MMEventEmitter: add enums for events

---
+++ react-native-meridian-maps/ios/MeridianCustom/MMEventNames.m
@@ -0,0 +1,26 @@
+#import "MMEventNames.h"
+
+NSString *const MMEventCustomEvent = @"MMCustomEvent";
+NSString *const MMEventMapLoadStart = @"onMapLoadStart";
+NSString *const MMEventMapLoadFinish = @"onMapLoadFinish";
+NSString *const MMEventMapLoadFail = @"onMapLoadFail";
+NSString *const MMEventLocationUpdated = @"onLocationUpdated";
+NSString *const MMEventMarkerSelect = @"onMarkerSelect";
+NSString *const MMEventMarkerDeselect = @"onMarkerDeselect";
+NSString *const MMEventError = @"onError";
+NSString *const MMEventMapTransformChange = @"onMapTransformChange";
+NSString *const MMEventOrientationUpdated = @"onOrientationUpdated";
+NSString *const MMEventDirectionsReroute = @"onDirectionsReroute";
+NSString *const MMEventDirectionsClick = @"onDirectionsClick";
+NSString *const MMEventDirectionsStart = @"onDirectionsStart";
+NSString *const MMEventRouteStepIndexChange = @"onRouteStepIndexChange";
+NSString *const MMEventDirectionsClosed = @"onDirectionsClosed";
+NSString *const MMEventDirectionsError = @"onDirectionsError";
+NSString *const MMEventUseAccessiblePathsChange = @"onUseAccessiblePathsChange";
+NSString *const MMEventMarkerForSelectedMarker = @"markerForSelectedMarker";
+NSString *const MMEventCalloutClick = @"onCalloutClick";
+NSString *const MMEventDirectionsCalculated = @"onDirectionsCalculated";
+NSString *const MMEventDirectionsRequestComplete = @"onDirectionsRequestComplete";
+NSString *const MMEventDirectionsRequestError = @"onDirectionsRequestError";
+NSString *const MMEventDirectionsRequestCanceled = @"onDirectionsRequestCanceled";
+NSString *const MMEventSearchActivityStarted = @"onSearchActivityStarted";
