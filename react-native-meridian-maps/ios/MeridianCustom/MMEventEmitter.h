543d6f9b918e93ad53a8dee4c830658c72130bfc
Iliya Lyan <68940374+12ya@users.noreply.github.com>
2025-05-22 00:56:03 +0900

    MMEventEmitter: update

--- react-native-meridian-maps/ios/MeridianCustom/MMEventEmitter.h
+++ react-native-meridian-maps/ios/MeridianCustom/MMEventEmitter.h
@@ -2,5 +2,5 @@
 #import <React/RCTBridgeModule.h>
 
 @interface MMEventEmitter : RCTEventEmitter <RCTBridgeModule>
-
+- (void)emitCustomEvent:(NSString *)eventName body:(NSDictionary *)body;
 @end
--- react-native-meridian-maps/ios/MeridianCustom/MMEventEmitter.m
+++ react-native-meridian-maps/ios/MeridianCustom/MMEventEmitter.m
@@ -1,13 +1,49 @@
 #import "MMEventEmitter.h"
+#import "MMEventNames.h"
 
 @implementation MMEventEmitter
   BOOL hasListeners;
 
+//
+//
+//+ (instancetype)sharedInstance {
+//  static MMEventEmitter *sharedInstance = nil;
+//  static dispatch_once_t onceToken;
+//  dispatch_once(&onceToken, ^{
+//    sharedInstance = [[self alloc] init];
+//  });
+//  return sharedInstance;
+//}
 
 RCT_EXPORT_MODULE();
 
-- (NSArray<NSString *> *)supportedEvents {
-  return @[@"MMCustomEvent"];
+  - (NSArray<NSString *> *)supportedEvents {
+  return @[
+    MMEventCustomEvent,
+    MMEventMapLoadStart,
+    MMEventMapLoadFinish,
+    MMEventMapLoadFail,
+    MMEventLocationUpdated,
+    MMEventMarkerSelect,
+    MMEventMarkerDeselect,
+    MMEventError,
+    MMEventMapTransformChange,
+    MMEventOrientationUpdated,
+    MMEventDirectionsReroute,
+    MMEventDirectionsClick,
+    MMEventDirectionsStart,
+    MMEventRouteStepIndexChange,
+    MMEventDirectionsClosed,
+    MMEventDirectionsError,
+    MMEventUseAccessiblePathsChange,
+    MMEventMarkerForSelectedMarker,
+    MMEventCalloutClick,
+    MMEventDirectionsCalculated,
+    MMEventDirectionsRequestComplete,
+    MMEventDirectionsRequestError,
+    MMEventDirectionsRequestCanceled,
+    MMEventSearchActivityStarted
+  ];
 }
 
 - (void)emitCustomEvent: (NSString *)eventName body: (NSDictionary *)body {
